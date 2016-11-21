3e9ce4d4a79294b6dc4646cf68db440f40a1ed81
Gabriel Gimenez <12-11006@usb.ve>
2016-11-21 17:39:57 -0400

    whtf

--- OGame UI++/script.js
+++ OGame UI++/script.js
@@ -913,43 +913,58 @@ var userscript = function() {
     else return num;
   }
 
-  function loadMessages(){
-    $.get("index.php", { page : "messages", tab : "20", ajax: "1"}).done(
-        function(data){
-          var object = $.parseHTML(data);
-          var curPage = $($(object).find(".curPage")[0]).text();
-          var current = curPage.split("/")[0];
-          var max = curPage.split("/")[1];
+  // function loadMessages(page = 1, limit = 1){
+  //   if ( page > limit ) {
+  //     return;
+  //   }
+  //   if (page === 1){
+  //     consig.messages = [];
+  //   }
+  //   console.log("Requesting page: " + page);
+  //   $.get("index.php", { page : "messages", pagination: page, tab : "20", ajax: "1"}).done(
+  //       function(data){
+  //         var object = $.parseHTML(data);
+  //         var curPage = $($(object).find(".curPage")[0]).text();
+  //         var current = curPage.split("/")[0];
+  //         var max = curPage.split("/")[1];
 
-          var messages = $(object).find("li.msg");
-          var message_arr = []
-          $(messages).each( function(){
-              var from = $(".msg_sender",$(this)).text();
-              var planet = $('.msg_title > a',$(this)).text().split(" ").pop();
-              var player = $('.msg_content > .compacting:first > span:eq(1)',$(this)).text().trim();
-              if( from === "Fleet Command"){
-                var stringr = $(this).wrap('<p/>').parent().html()
-                message_arr.push(stringr);
-              }
-            }
-          );
-          config.messages = message_arr;
-          saveConfig(config);
-          console.log(message_arr[0]);
-        }
-
-    );
-  }
-  //loadMessages();
+  //         var messages = $(object).find("li.msg");
+  //         var message_arr = []
+  //         $(messages).each( function(){
+  //             var id = $(this).attr("data-msg-id");
+  //             if ( config.messages.length > 0 && config.messages[0].id_message == id){
+  //               return false;
+  //             }
+  //             var from = $(".msg_sender",$(this)).text();
+  //             var planet = $('.msg_title > a',$(this)).text().split(" ").pop();
+  //             var player = $('.msg_content > .compacting:first > span:eq(1)',$(this)).text().trim();
+  //             if( from === "Fleet Command"){
+  //               var stringr = $(this).wrap('<p/>').parent().html()
+  //               message_arr.push(
+  //                 {id_message: id,
+  //                  message: stringr,
+  //                  planet: planet,
+  //                  player: player}
+  //                 );
+  //             }
+  //           }
+  //         );
+  //         loadMessages(page+1, max);
+  //        // config.messages = config.messages.concat(message_arr);
+  //         saveConfig(config);
+  //       }
+  //   );
+  // }
+  // //loadMessages();
 
   window.filterMessages = function() {
     var name = $("#Player").val();
     var wrapper = $("#messageBit").html(name);
     for (var i = 0; i < config.messages.length; i++) {
-      var player = $('.msg_content > .compacting:first > span:eq(1)',$(config.messages[i])).text().trim();
-      var planet = $('.msg_title > a',$(config.messages[i])).text().split(" ").pop();
+      var player = config.messages[i].player;
+      var planet = config.messages[i].planet;
       if (player.toUpperCase().indexOf(name.toUpperCase()) !== -1 || planet.indexOf(name) !== -1){
-        var el = $(config.messages[i]);
+        var el = $(config.messages[i].message);
         wrapper.append(el);
       }
     }
@@ -967,7 +982,7 @@ var userscript = function() {
     var adder = $('<div class="addFav"> <a>Player</a><input value="" id="Player" style="width:20%;" type="text"> <a onclick="filterMessages()">Filter</a></div>');
     var wrapper = $('<div id="messageBit" class="uiEnhancementWindow"></div>');
     for (var i = 0; i < config.messages.length; i++) {
-      var el = config.messages[i];
+      var el = config.messages[i].message;
       wrapper.append($(el));
     }
     
