891484f83c8b4c6f95b9f0cf91cc0f29a801dd45
Erwan Beauvois <erwan@allmyt.eu>
2017-06-13 22:25:16 +0200

    Fix data reset button

--- OGame UI++/src/utils/persistent-config.js
+++ OGame UI++/src/utils/persistent-config.js
@@ -2,6 +2,7 @@ var fn = function () {
   'use strict';
   window._getConfig = getConfig;
   window._saveConfig = saveConfig;
+  window._resetConfig = resetConfig;
 
   function saveConfig(config) {
     if (typeof (Storage) !== 'undefined') {
@@ -11,9 +12,16 @@ var fn = function () {
 
   function getConfig() {
     if (typeof (Storage) !== 'undefined') {
-      return JSON.parse(localStorage.getItem('og-enhancements')) || {};
+      return JSON.parse(localStorage.getItem('og-enhancements') || '{}');
     } else {
       return null;
+    }
+  }
+
+  function resetConfig() {
+    if (typeof (Storage) !== 'undefined') {
+      localStorage.setItem('og-enhancements', '{}');
+      config = {};
     }
   }
 };
