9874df946e327105458372f20db648ef04caa84e
pawelzwronek <pawel.z.wronek@gmail.com>
2017-07-20 04:13:44 +0200

    A: enque spy mission to avoid too fast clicking A: wait for free mission slot

--- OGame UI++/src/utils/send-spy-probes.js
+++ OGame UI++/src/utils/send-spy-probes.js
@@ -1,30 +1,163 @@
 var fn = function () {
   'use strict';
+
+  window._startTime = Date.now();
   window._spy = function (galaxy, system, position) {
-    $.ajax('?page=minifleet&ajax=1', {
-      data: {
-        mission: 6,
-        galaxy: galaxy,
-        system: system,
-        position: position,
-        type: 1,
-        shipCount: window.constants.espionage,
-        token: window.miniFleetToken
-      },
-      dataType: 'json',
-      type: 'POST',
-      success: function (a) {
-        if (a.response.success) {
-          $('#planet_' + galaxy + '_' + system + '_' + position + ' .icon_eye').addClass('disabled');
-          window.uipp_analytics('uipp-spy', 'success');
-        } else {
-          window.fadeBox(a.response.message, true);
-          window.uipp_analytics('uipp-spy', 'failed');
+
+    function msg (str) {
+      window.fadeBox(str, false);
+      log('msg:' + str);
+    }
+
+    function log (str) {
+      str;
+      // window.console.log((Date.now() - window._startTime) + ': ' + str);
+    }
+
+    function Spy (coords) {
+      var _this = this;
+      _this.coords = coords;
+      _this.coordsStr = coords.galaxy + '_' + coords.system + '_' + coords.position;
+
+      _this.setStatus = function (status) {
+        function setColor (color) {
+          $('#planet_' + _this.coordsStr + ' .icon_eye').css('box-shadow', 'inset 0 0 0 20px rgba(' + color + ',0.3)');
         }
 
-        window.miniFleetToken = a.newToken;
+        if (status === 'new') {
+          setColor('0,100,250');
+        } else if (status === 'spy') {
+          setColor('0,100,0');
+        } else if (status === 'delay') {
+          setColor('200,200,0');
+        } else if (status === 'end') {
+          setColor('0,200,0');
+        } else if (status === 'error') {
+          setColor('250,0,0');
+        }
+        _this.status = status;
+        log(_this.coordsStr + ' status: ' + status);
+      };
+
+      _this.eq = function (coords1) {
+        return coords1.galaxy === _this.coords.galaxy &&
+                coords1.system === _this.coords.system &&
+                coords1.position === _this.coords.position;
+      };
+
+      _this.elapsed = function () {
+        return Date.now() - _this.spyTime;
+      };
+
+      _this.spy = function (delay) {
+        if (delay && delay > 0) {
+          _this.setStatus('delay');
+          setTimeout(_this.spy, delay);
+          return;
+        }
+
+        _this.spyTime = Date.now();
+        _this.setStatus('spy');
+        $.ajax('?page=minifleet&ajax=1', {
+          data: {
+            mission: 6,
+            galaxy: galaxy,
+            system: system,
+            position: position,
+            type: 1,
+            shipCount: window.constants.espionage,
+            token: window.miniFleetToken
+          },
+          dataType: 'json',
+          type: 'POST',
+          success: function (a) {
+            if (a.response.success) {
+              _this.setStatus('end');
+              log('success ' + _this.coordsStr);
+              processSpyQueue();
+              window.uipp_analytics('uipp-spy', 'success');
+            } else {
+              _this.setStatus('delay');
+              log(a.response.message);
+              window.uipp_analytics('uipp-spy', 'failed');
+              if (a.response.coordinates) {
+                // wait for free mission slot
+                enqueueOnFreeMissionSlot(_this.spy);
+              } else {
+                // general error, try again in 2 sec
+                setTimeout(_this.spy, 2000);
+              }
+            }
+            window.miniFleetToken = a.newToken;
+          }
+        });
+      };
+
+      _this.setStatus('new');
+    }
+
+    function enqueueOnFreeMissionSlot (func) {
+      // get end time of next mission
+      $.ajax('?page=eventList&ajax=1', {
+        dataType: 'html',
+        type: 'POST',
+        success: function (res) {
+          var timeTab = [];
+          $(res).find('tr.eventFleet').each(function () {
+            var t = $(this).attr('data-arrival-time');
+            if (t && $(this).attr('data-return-flight') === 'true') {
+              log('data-arrival-time: ' + t);
+              timeTab.push(t);
+            }
+          });
+          if (timeTab.length > 0) {
+            timeTab.sort(function (a, b) {return a - b;});
+            log('timeTab: ' + Date.now() / 1000 + ' ' + JSON.stringify(timeTab));
+            var waitTime = Math.round(timeTab[0] - Date.now() / 1000) + 1;
+            msg('Waitng ' + waitTime + ' seconds for free mission slot...');
+            setTimeout(func, waitTime * 1000);
+          } else {
+            func();
+          }
+        }
+      });
+    }
+
+    function enqueueSpy (coords) {
+      window.spyQueue = window.spyQueue || [];
+      if (!window.spyQueue.find(function (s) { return s.eq(coords); })) {
+        window.spyQueue.push(new Spy(coords));
+        processSpyQueue();
       }
-    });
+    }
+
+    function processSpyQueue () {
+      if (window.spyQueueCurrent && window.spyQueueCurrent.status === 'end') {
+        // remove from queue
+        window.spyQueue = window.spyQueue.filter(function (s) { return s.spyTime !== window.spyQueueCurrent.spyTime; });
+      }
+
+      if (!window.spyQueueCurrent || window.spyQueueCurrent.status === 'end') {
+        var spy = window.spyQueue.find(function (s) { return s.status === 'new'; });
+        if (spy) {
+          var delay = 0;
+          if (window.spyQueueCurrent)
+            delay = Math.max(0, 1500 - window.spyQueueCurrent.elapsed());
+          spy.spy(delay);
+          window.spyQueueCurrent = spy;
+        } else {
+          log('no new spy request ' + window.spyQueue.length);
+        }
+      } else {
+        if (window.spyQueue.length > 0) {
+          log('spy in progress: ' + JSON.stringify(window.spyQueueCurrent));
+        } else {
+          log('spyQueue empty: ');
+        }
+      }
+    }
+
+    enqueueSpy({ galaxy: galaxy, system: system, position: position });
   };
 };
 
