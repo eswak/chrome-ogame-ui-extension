ff047cb3ec4b401f5105035254982908ba670eea
Erwan Beauvois <erwan@allmyt.eu>
2020-02-10 19:05:50 +0100

    Add solar satellite counter to mines energy consumption

--- OGame UI++/src/solarsat-helper.js
+++ OGame UI++/src/solarsat-helper.js
@@ -2,6 +2,9 @@ var fn = function () {
   'use strict';
   window._addSolarSatHelperInterval = function _addSolarSatHelperInterval () {
     setInterval(function () {
+      // ==========================================================================
+      // Auto-fill number of solar satellites
+      // ==========================================================================
       var $el = $('#technologydetails .solarSatellite:not(.enhanced)');
 
       if ($el.length) {
@@ -11,12 +14,29 @@ var fn = function () {
         if (energy < 0) {
           var production = Number($('.energy_production .bonus').attr('data-value'));
           if (isNaN(production)) {
-			  return;
-		  }
+    			  return;
+    		  }
           var n = Math.ceil(Math.abs(energy) / production);
           $('#build_amount')[0].value = n;
         }
       }
+
+      // ==========================================================================
+      // Solar satellite counter near energy consumption
+      // ==========================================================================
+      var energyProducedPerSat = Math.floor((config.my.planets['[' + _getCurrentPlanetCoordinates().join(':') + ']'].averageTemp + 160) / 6);
+      $('li.additional_energy_consumption:not(.enhanced)').each(function() {
+        $(this).addClass('enhanced');
+
+        var additionalEnergyNeeded = Number($(this).find('.value').attr('data-value'));
+        var nSats = Math.ceil(additionalEnergyNeeded / energyProducedPerSat);
+        $(this).append([
+          '<span class="enhancement tooltip" style="font-weight:bold;margin-left:5px" title="' + $('.technology.solarSatellite').attr('aria-label') + ': ' + nSats + '">(',
+          '<img src="' + uipp_images.ships[212] + '" style="height:16px;vertical-align:-4px"/> ',
+          nSats,
+          ')</span>'
+        ].join(''));
+      });
     }, 100);
   };
 };
